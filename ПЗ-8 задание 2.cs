8543dc1dc9baa80ad841e7c0e2298893fcc5945e
KorobcenkoAleksandr <sasakorobcenko049@gmail.com>
2024-10-13 16:47:06 +0300

    Add files via upload

---
+++ ПЗ-8 задание 2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+internal class program2
+{
+    static void ain(string[] args)
+    {
+Console.WriteLine("введите число от 5 до 10");
+int num = Convert.ToInt32(Console.ReadLine());
+
+    if (num <= 10 & num >= 5)
+    {
+        Console.WriteLine("число больше 5 и меньше 10");
+    }
+    else
+    {
+        Console.WriteLine("неизвестное число");
+    }
+    }
+    }
+
