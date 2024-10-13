9a710d01787455026843ffb1350c0b9b19062afd
KorobcenkoAleksandr <sasakorobcenko049@gmail.com>
2024-10-13 22:37:23 +0300

    ПЗ-10 задание2.cs

--- ПЗ-10 задание2.cs
+++ ПЗ-10 задание2.cs
@@ -4,27 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Введите последовательность целых чисел 0 для завершения:");
+        Console.WriteLine("Введите последовательность целых чисел для завершения:");
 
-        int number;
-        int maxNumber = int.MinValue; 
-        int maxNumberIndex = 0; 
+        int num;
+        int maxNum = int.MinValue; 
+        int maxNumIndex = 0; 
         int currentIndex = 0; 
 
         do
         {
-            number = Convert.ToInt32(Console.ReadLine());
+            num = Convert.ToInt32(Console.ReadLine());
 
-            if (number != 0 && number > maxNumber)
+            if (num != 0 & number > maxNumber)
             {
-                maxNumber = number;
-                maxNumberIndex = currentIndex;
+                maxNum = number;
+                maxNumIndex = currentIndex;
             }
 
             currentIndex++; 
 
-        } while (number != 0); 
+        } while (num != 0); 
 
-        Console.WriteLine($"Максимальное число: {maxNumber}, порядковый номер: {maxNumberIndex + 1}");
+        Console.WriteLine($"Максимальное число: {maxNum}, порядковый номер: {maxNumIndex + 1}");
     }
 }
